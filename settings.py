0e695f5069470676e8bfc0f34db13e889222839d
madskillskill <mikeitkulof@gmail.com>
2023-12-11 17:14:21 +0400

    GUI is 25% complete

--- settings.py
+++ settings.py
@@ -1,10 +1,28 @@
-last_directory = 'sensitive/'
-csv_file = ''
-json_file = ''
-google_sheet_name = ''
+from tkinter import StringVar, Tk
 
-accept_button = ''
-verify_button = ''
-compare_button = ''
-export_button = ''
-close_button = ''
+root = Tk()
+last_directory = StringVar()
+csv_file = StringVar()
+json_file = StringVar()
+google_sheet_name = StringVar()
+
+accept_button = StringVar()
+verify_button = StringVar()
+compare_button = StringVar()
+export_button = StringVar()
+close_button = StringVar()
+load_output = StringVar()
+
+###
+
+last_directory.set('sensitive/')
+csv_file.set('')
+json_file.set('')
+google_sheet_name.set('VIII Гордеевский фестиваль')
+
+accept_button.set('')
+verify_button.set('')
+compare_button.set('')
+export_button.set('')
+close_button.set('')
+load_output.set('none')
